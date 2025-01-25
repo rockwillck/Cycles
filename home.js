01cdf736603683ca4c5ac6b7e0e297d85dd10e1d
William Choi-Kim <will.choi.kim@gmail.com>
2025-01-24 23:12:54 -0600

    small changes

--- home.js
+++
@@ -1,9 +0,0 @@
-let installPrompt = null;
-const installButton = document.querySelector("#install");
-
-window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  console.log("hi")
-  installPrompt = event;
-  installButton.removeAttribute("hidden");
-});
